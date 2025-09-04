d935d0dbb9660632b901666f57ef831949d64f3a
alsh2007 <109054196+alsh2007@users.noreply.github.com>
2025-09-04 04:40:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,10 @@ def download_audio(url):
         "quiet": True,
         "nocheckcertificate": True,
         "geo_bypass": True,
-        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+
+        # 👇 هذا السطر خليته يقرأ الكوكيز من ملف
+        "cookiefile": "cookies.txt"
     }
 
     files = []  
